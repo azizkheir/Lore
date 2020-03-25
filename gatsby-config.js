508f4ccff699674e57d7b64b6a9d13535015ba0f
azizkheir <61793265+azizkheir@users.noreply.github.com>
2020-03-25 14:32:39 -0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,71 @@
+module.exports = {
+  siteMetadata: {
+    title: `Lore`,
+    author: ``,
+    description: `A Chicago based CBD co`,
+    siteUrl: `https://dreamy-morse-e8c5f6.netlify.com`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/items`,
+        name: `items`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Snipcart Starter`,
+        short_name: `Gatsby Snipcart`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `thistle`,
+        display: `minimal-ui`,
+        icon: `content/assets/lore_thumbnail.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-snipcart",
+      options: {
+        //replace with own Snipcart API key
+        apiKey:
+          "MGY2NjEyNzItNDFmMy00NjUxLThkY2MtYjc4OGFlMTYyMGI0NjM3MjA2MTEyMjcwMjYxNjk5",
+        autopop: true,
+      },
+    },
+    `gatsby-plugin-styled-components`,
+  ],
+}
---
+++ src/components/layout.js
@@ -0,0 +1,69 @@
+import React from "react"
+import styled, { ThemeProvider } from "styled-components"
+import { theme } from "../styles/theme"
+import { GlobalStyle } from "../styles/globalStyle"
+
+import HeaderMain from "./Headers/HeaderMain"
+import HeaderMinor from "./Headers/HeaderMinor"
+
+const PageWrapper = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  width: 90%;
+`
+
+const MainSection = styled.main`
+  margin: 30px 0;
+  width: 100%;
+`
+
+const FooterStyled = styled.footer`
+  width: 100%;
+  padding: 20px;
+  text-align: right;
+
+  @media (max-width: 600px) {
+    text-align: center;
+  }
+`
+const ExternalLink = styled.a`
+  color: #c59fc5;
+`
+
+class Layout extends React.Component {
+  componentDidMount() {
+    if (window.Snipcart) {
+      window.Snipcart.api.configure("show_continue_shopping", true)
+    }
+  }
+
+  render() {
+    const { location, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+    const siteName = "Lore"
+    let header
+
+    if (location.pathname === rootPath) {
+      header = <HeaderMain shopName={siteName} />
+    } else {
+      header = <HeaderMinor shopName={siteName} />
+    }
+
+    return (
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <PageWrapper>
+            {header}
+            <MainSection>{children}</MainSection>
+            <FooterStyled>
+              <strong>Local delivery coming soon</strong>
+            </FooterStyled>
+          </PageWrapper>
+        </>
+      </ThemeProvider>
+    )
+  }
+}
+
+export default Layout
